c037b7239109bb4298ac2af9ea6306f0be31d69f
kazuhiro-f <kz7260wanderer@gmail.com>
2021-02-21 10:44:17 +0000

    [add]connect to database

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@
 FROM golang:1.15.8
 
 # コンテナ内にディレクトリを作成
-RUN mkdir /go/src/go-docker-sample
+# エイリアスの定義
+RUN mkdir /go/src/go-docker-sample \
+	&& alias ll='ls -al'
 
 #コンテナログイン時のディレクトリを設定
 WORKDIR /go/src/go-docker-sample
--- main.go
+++ main.go
@@ -1,7 +1,52 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+type User struct {
+	gorm.Model
+	name string
+}
 
 func main() {
-	fmt.Println("Hello from go-docker-sample.")
+	DBMigrate(DBConnect())
+
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", hello())
+
+	err := e.Start(":8000")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func hello() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello from echo.")
+	}
+}
+
+func DBMigrate(db *gorm.DB) *gorm.DB {
+	db.AutoMigrate(&User{})
+	return db
+}
+
+func DBConnect() *gorm.DB {
+	db, err := gorm.Open("postgres", "host=postgres port=5432 user=example_user dbname=example_db password=password sslmode=disable")
+	if err != nil {
+		panic(err)
+	}
+	return db
 }
